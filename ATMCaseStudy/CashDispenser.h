b642414614f18131186760e3885f113c579cd792
mankaa <mankaache10@gmail.com>
2023-01-02 05:09:24 +0100

    cashdispenser

--- ATMCaseStudy/CashDispenser.h
+++ ATMCaseStudy/CashDispenser.h
@@ -6,10 +6,38 @@
 #define ATM_MACHINE_CASHDISPENSER_H
 
 
+#include <string>
+class CAshDispenser {
+private:
+    // the default initial number of bills in the cash dispenser
+    const static int INITIAL_COUNT = 500;
+    int count;// number of $20 bills remaining
 
-class CashDispenser {
+public:
+    // no-arguement CashDispenser constructor initializes count to default
+    CAshDispenser() {
+        count = INITIAL_COUNT;
+    }
 
-};
+    // simulates dispensing of specific amount of cash
+    void dispenseCash(int amount) {
+        int billsRequired = amount / 20; // number of $20 bills required
+        count -= billsRequired;// update the count of bills
+    }
+
+    // indicates whether cash dispenser can dispense desired amount
+    bool isSufficientCashAvailable(int amount){
+        int billsRequired = amount / 20;// number of $20 bills required
+
+        if(count >= billsRequired){
+            return true; // enough bills available
+        }else{
+            return false;// not enough bills available
+        }
+
+    }
+
+};//end class CashDispenser
 
 
 

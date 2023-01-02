da51e5301ee13d1eee724c26f749a5bfe96d3516
mankaa <mankaache10@gmail.com>
2023-01-02 05:06:49 +0100

    cashdispenser

---
+++ ATMCaseStudy/CashDispenser.h
@@ -0,0 +1,16 @@
+//
+// Created by MADES on 02/01/2023.
+//
+
+#ifndef ATM_MACHINE_CASHDISPENSER_H
+#define ATM_MACHINE_CASHDISPENSER_H
+
+
+
+class CashDispenser {
+
+};
+
+
+
+#endif //ATM_MACHINE_CASHDISPENSER_H
